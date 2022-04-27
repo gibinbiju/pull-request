9a3279c532190b5bb15a553c10a00e6d551eb840
gibinkanattu <gibinbiju73@gmail.com>
2022-04-27 10:53:25 +0530

    performance

--- test-python.py
+++ test-python.py
@@ -1,5 +1,10 @@
+import datetime,time
 def main():
+    start_time=datetime.datetime.utcnow()
     print("hello world")
+    time.sleep(3)
+    end_time=datetime.datetime.utcnow()
+    print("performance={}".format(end_time-start_time))
 
 if __name__=='__main__':
     main()
